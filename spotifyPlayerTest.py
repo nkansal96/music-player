644e5af520599635fb77a2e5cd34f2690a649932
Nathan Yang <nathanyang@engineering.ucla.edu>
2018-03-11 23:11:37 -0700

    Modified spotifyPlayerTest

--- spotifyPlayerTest.py
+++ spotifyPlayerTest.py
@@ -3,7 +3,9 @@ import settings
 
 def main():
 	p = SpotifyPlayer(settings.SPOTIFY_CLIENT_ID, settings.SPOTIFY_CLIENT_SECRET)
-	p.play_song("Summer", "Marshmello")
+	# p.play_song("Summer", "Marshmello")
+	# p.play_playlist("USA Top 50")
+	p.play_artist("Marshmello")
 	while True:
 		pass
 
