e6aefedfe92c4d7ef091416c8b42bb6e1724f788
Helen Lee <helenhyewonlee@gmail.com>
2018-03-12 15:53:38 -0700

    made it compile

--- aurora.py
+++ aurora.py
@@ -1,15 +1,15 @@
-import auroraapi as auroraapi
+import auroraapi as aurora
 from auroraapi.audio import AudioFile
 from auroraapi.speech import Speech, continuously_listen
 
 import spotifyPlayer as player
 
-aurora.config.add_id = ""
-aurora.config.app_token = ""
+aurora.config.app_id = "ceb5d7c34056489150660ec5ebcc1c5f"
+aurora.config.app_token = "6NWvZigglXLlmA4aD1k0iJctnNYigyG"
 trigger_word = ""
 
 valid_words = ["song", "artist", "volume"]
-valid_entities = lambda d: "object" in d and d["object"] in valid_words
+# valid_entities = lambda d: "object" in d and d["object"] in valid_words
 
 def listen():
 
@@ -17,12 +17,13 @@ def listen():
 		text = speech.text()
 		print(text.text)
 
-		if trigger_word in text.text:
+		if len(text.text) > 0:
 			i = text.interpret()
 			print(i.intent)
 
-			if i.intent == "play_song" and valid_entities(i.entities):
+			if i.intent == "play_song" :
 				title = i.entities["song"]
+				print(title)
 				if "artist" in i.entities:
 					artist = i.entities["artist"]
 				else:
@@ -51,7 +52,8 @@ def listen():
 				player.volume(volume)
 
 
-
+if __name__ == "__main__":
+	listen()
 
 
 
