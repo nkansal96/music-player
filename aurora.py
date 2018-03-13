d6082e080073f3cbb49c947f7f1d909e9fa75785
Helen Lee <helenhyewonlee@gmail.com>
2018-03-12 17:16:38 -0700

    edits again

--- aurora.py
+++ aurora.py
@@ -1,54 +1,63 @@
 import auroraapi as aurora
-from auroraapi.audio import AudioFile
-from auroraapi.speech import Speech, continuously_listen
+import settings
 
-import spotifyPlayer as player
+from auroraapi.audio import AudioFile
+from auroraapi.speech import Speech, continuously_listen_and_transcribe
+from auroraapi.interpret import Interpret
+from spotifyPlayer import SpotifyPlayer
+
+
 
 aurora.config.app_id = "ceb5d7c34056489150660ec5ebcc1c5f"
 aurora.config.app_token = "6NWvZigglXLlmA4aD1k0iJctnNYigyG"
 trigger_word = ""
 
-valid_words = ["song", "artist", "volume"]
+# valid_words = ["song", "artist", "volume"]
+player = SpotifyPlayer(settings.SPOTIFY_CLIENT_ID, settings.SPOTIFY_CLIENT_SECRET)
 # valid_entities = lambda d: "object" in d and d["object"] in valid_words
 
 def listen():
 
-	for speech in continuously_listen(silence_len=1):
-		text = speech.text()
-		print(text.text)
+	for text in continuously_listen_and_transcribe(silence_len=0.5):
 
 		if len(text.text) > 0:
 			i = text.interpret()
-			print(i.intent)
 
-			if i.intent == "play_song" :
+			if i.intent == "play_song":
+				if "song" not in i.entities:
+					continue 
 				title = i.entities["song"]
-				print(title)
 				if "artist" in i.entities:
 					artist = i.entities["artist"]
 				else:
 					artist = ""
 				player.play_song(title, artist)
+				pass
 
-			if i.intent == "play_artist" and valid_entities(i.entities):
+			if i.intent == "play_artist":
 				artist = i.entities["artist"]
 				player.play_song("", artist)
 				pass
 
-			if i.intent == "play_playlist" and valid_entities(i.entities):
+			if i.intent == "play_playlist":
 				pass
 
-			if i.intent == "play_next" and valid_entities(i.entities):
+			if i.intent == "play_next":
 				pass
 
-			if i.intent == "pause" and valid_entities(i.entities):
+			if i.intent == "pause":
 				player.pause()
+				pass
 
-			if i.intent == "resume" and valid_entities(i.entities):
+			if i.intent == "resume":
 				player.resume()
+				pass
 
-			if i.intent == "volume" and valid_entities(i.entities):
-				volume = i.entities["volume"] #need to cast this as int
+			if i.intent == "volume":
+				print(i.entities)
+				if "volume" not in i.entities or not i.entities["volume"].isdigit():
+					continue 
+				volume = int(i.entities["volume"])
 				player.volume(volume)
 
 
