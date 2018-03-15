ec924493b03db42ee924c7480430ac528a933e20
Nikhil Kansal <nkansal96@gmail.com>
2018-03-15 12:00:33 -0700

    duck should only duck to lower volume

--- test_utils.py
+++ test_utils.py
@@ -12,18 +12,32 @@ class TestDuck(object):
 		ov = self.player.player.volume
 		with utils.duck(self.player, 20) as d:
 			assert d.volume == d.original_vol
-			assert d.original_vol == self.player.player.volume
+			assert d.volume == self.player.player.volume
 			assert ov == self.player.player.volume
 		assert self.player.player.volume == ov
-	
+
 	def test_duck_playing(self):
 		self.player.play_song("hello", "adele")
-		time.sleep(2.0)
+		time.sleep(1.0)
 		ov = self.player.player.volume
 		with utils.duck(self.player, 5) as d:
 			assert d.volume == 5
 			assert self.player.player.volume == 5
-			time.sleep(1.0)
+			time.sleep(0.5)
 		assert self.player.player.volume == ov
-		time.sleep(1.0)
+		time.sleep(0.5)
 		self.player.player.stop()
+
+	def test_duck_volume_higher(self):
+		# this should not affect the volume
+		self.player.play_song("hello", "adele")
+		time.sleep(2.0)
+		ov = self.player.player.volume
+		with utils.duck(self.player, 80) as d:
+			assert d.volume == d.original_vol
+			assert d.volume == self.player.player.volume
+			time.sleep(0.5)
+		assert self.player.player.volume == ov
+		time.sleep(0.5)
+		self.player.player.stop()
+	
--- utils.py
+++ utils.py
@@ -10,17 +10,15 @@ class duck(object):
 		self.original_vol = player.player.volume
 	
 	def __enter__(self):
-		step = -self.step_size if self.target < self.original_vol else self.step_size
-		target = self.target - 1 if self.target < self.original_vol else self.target + 1
-		for i in range(self.original_vol, target, step):
+		if self.target >= self.player.player.volume:
+			return self
+		for i in range(self.original_vol, self.target-1, -self.step_size):
 			self.player.volume(i)
 			time.sleep(self.step_interval)
 		self.volume = self.player.player.volume
 		return self
 
 	def __exit__(self, type, value, traceback):
-		step = -self.step_size if self.target > self.original_vol else self.step_size
-		orig = self.original_vol - 1 if self.target > self.original_vol else self.original_vol + 1
-		for i in range(self.target, orig, step):
+		for i in range(self.target, self.original_vol + 1, self.step_size):
 			self.player.volume(i)
 			time.sleep(self.step_interval)
